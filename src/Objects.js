34c9924560a6aed150940112d3a7edcbf80ceb42
Alexandr_Tabanakov <systema.com2@mail.ru>
2022-03-22 22:20:43 +0300

    Объекты

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,16 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data.gender == 'female') {
+        if (data.hasOwnProperty('age')) {
+            delete data.age;
+        }
+    } else {
+        if (!data.hasOwnProperty('income')) {
+            data.income = 100000;
+        }
+    }
+    return data;
 }
 
 /*
@@ -15,6 +25,16 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    let arr = [];
+
+    for (let key in obj1) arr.push(key);
+
+    for (let key in obj2) arr.push(key);
+
+    for (let key in obj3) arr.push(key);
+
+    arr.sort();
+    return arr;
 }
 
 /*
@@ -23,4 +43,11 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let arr = [];
+    for (let i = 0; i < count; i++) {
+        let clone = JSON.parse(JSON.stringify(obj));
+        clone.id = i;
+        arr.push(clone);
+    }
+    return arr;
 }
